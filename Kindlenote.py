c261b9bb4202bf6f3ef2f75abe769a0805792455
Lugghawk <acidictadpole@gmail.com>
2010-12-14 12:53:46 -0500

    Added some better error handling. Added password prompt with shadowing

--- Kindlenote.py
+++ Kindlenote.py
@@ -1,18 +1,12 @@
+#!/usr/bin/python
 import smtplib
 import sys
 import os
+import getpass
 
-##Check for --help etc
-##Need to do this first since they may --help without moving over their config. If they do, we shouldn't bombard them with errors about a missing config, since we don't really need it to show them the help.
-sys.argv.pop(0) # Get rid of the script name, we don't need it.
-#Check for 'Convert' flag.
-if sys.argv[0] == '-c':
-	#Convert!
-	if DEBUG:
-		print "Convert flag found!"
-	convert = True #We pass this to our sendMail function so that our subject line contains the word Convert.
-	sys.argv.pop(0)
-elif sys.argv[0] == '--help':
+from socket import error as sockError
+
+def printHelp():
 	print """
 	Usage: 
 	
@@ -27,6 +21,20 @@ elif sys.argv[0] == '--help':
 	Your config file, config.py, needs to be filled in. Take config_sample.py and copy it to config.py, fill in your smtp details, and fire away.
 	"""
 	sys.exit(0)
+
+if len (sys.argv) == 1: # Just the name of the program, no arguments.
+	printHelp()	# Display the help page.
+
+sys.argv.pop(0) # Get rid of the script name, we don't need it.
+#Check for 'Convert' flag.
+if sys.argv[0] == '-c':
+	#Convert!
+	if DEBUG:
+		print "Convert flag found!"
+	convert = True #We pass this to our sendMail function so that our subject line contains the word Convert.
+	sys.argv.pop(0)
+elif sys.argv[0] == '--help':
+	printHelp()
 
 ##Continue with imports
 try:
@@ -52,7 +60,8 @@ DEBUG = True # Sets our debug status to true. Will give some extra logging throu
 
 
 def checkConfig():
-	if config.smtpServer == '' or config.userName == '' or config.passWord == '' or config.to_addr == '' or config.from_addr == '':
+	if config.smtpServer == '' or config.userName == '' or config.to_addr == '' or config.from_addr == '':
+		#Make sure the required items are there.
 		print "There was a problem with your config. One or more of the required values are not set."
 		config.configFile = False
 
@@ -61,6 +70,8 @@ def checkConfig():
 	else:
 		print "Please correct your config.py file and try again"
 		sys.exit(0) # Exits the program since the config file is not right.
+			
+	
 ################################################
 ##Function Declarations##
 def sendMail(from_addr, to_addr, mesg, smtpObject,attach=[], convertFlag = False):
@@ -104,6 +115,9 @@ def sendMail(from_addr, to_addr, mesg, smtpObject,attach=[], convertFlag = False
 
 	
 def smtpLogin(userName, passWord, smtpObject):
+	if passWord == '':
+		#The password wasn't set in the config file. We need to prompt the user for it.
+		passWord = getpass()
 	try:
 		smtpObject.login(userName, passWord)
 		del(userName) # Can't be too careful.
@@ -116,19 +130,21 @@ def smtpLogin(userName, passWord, smtpObject):
 		
 def checkArgs (arguments): # Checks files that are specified at command line exist. Adds all command line items to a stack so it only has to loop once.
 	filesToSend = []
-	filesDontExist = False
 	for arg in arguments:
+	
 		try:
 			open(arg,"r")
 		except IOError:
-			print "File " + arg +" doesn't exist"
-			filesDontExist = True
-		filesToSend.append (arg)
+			print "File " + arg +" doesn't exist. Skipping."
+			#filesDontExist = True #Removing since we don't need to exit, just say that we're skipping that file.
+		else:
+			filesToSend.append (arg)
 	
-	if filesDontExist:
-		print "Exiting"
+	if len(filesToSend) == 0:
+		print "Exiting since no files listed" # Shouldn't happen, but just in case.
 		sys.exit(0) # Exiting due to a parameter's files not existing.
-	
+	elif len(filesToSend) >= 5:
+		print "Exiting. Too many files specified. Maximum of 5 allowed."
 	return filesToSend
 	
 	
@@ -144,12 +160,18 @@ checkConfig()
 try:
 	server = smtplib.SMTP_SSL(config.smtpServer,465) # Use SSL by default atm.
 	server.ehlo_or_helo_if_needed()
+except sockError as e:
+	print e
+	sys.exit(0)
 except (smtplib.SMTPException,smtplib.SMTPConnectError):
-	"There was a problem connecting"
+	print "There was a problem connecting"
+	sys.exit(0)
 else:
 	if DEBUG:
 		print "Connection to " + config.smtpServer + " successful"
-	
+
+		
+
 smtpLogin(config.userName, config.passWord, server)
 
 #Count the files on command line.
